8b1bd26de5e1e0c74abd9f9f546a9e6d753bf9b4
Urs-Praveen <work.erpraveen@gmail.com>
2023-04-12 17:10:39 +0530

    implemented transactions fixed bugs delete

--- controllers/expenseController.js
+++ controllers/expenseController.js
@@ -1,5 +1,6 @@
 const expenseData = require('../models/expenseData')
 const User = require('../models/loginPageModel')
+const sequelize = require('../util/database')
 
 exports.getExpense = async (req, res) => {
     try {
@@ -17,22 +18,26 @@ exports.getExpense = async (req, res) => {
 }
 exports.postExpense = async (req, res) => {
     try {
+        const t = await sequelize.transaction();
         const { description, amount, category } = req.body
         const dataFromBack = req.user.createExpense({  // magic sequelize operation 
             description,
             amount,
             category,
             //logindatumId: req.user.id
-        })
+        }, { transaction: t })
         const totalExpense = parseInt(req.user.totalExpense) + parseInt(amount)
         const update = User.update({
             totalExpense: totalExpense
         }, {
-            where: { id: req.user.id }
+            where: { id: req.user.id },
+            transaction: t
         })
         const expense = await Promise.all([dataFromBack, update])
+        await t.commit()
         res.status(201).json({ dataFromBack: expense })
     } catch (err) {
+        await t.rollback()
         console.log(err)
         res.status(500).json({ err: err })
     }
@@ -43,9 +48,21 @@ exports.deleteExpense = async (req, res) => {
         const id = req.params.id
         const userId = req.user.id
         console.log(userId)
-        await expenseData.destroy({ where: { id: id, logindatumId: userId } })
+        const t = await sequelize.transaction()
+        const expense = await expenseData.findOne({ where: { id: id, logindatumId: userId }, transaction: t })
+        console.log(expense.amount)
+        const totalExpense = parseInt(req.user.totalExpense) - parseInt(expense.amount)
+        await User.update({
+            totalExpense: totalExpense
+        }, {
+            where: { id: req.user.id },
+            transaction: t
+        })
+        await expenseData.destroy({ where: { id: id, logindatumId: userId }, transaction: t })
+        await t.commit()
         res.status(200).json({ res: "delete success" })
     } catch (err) {
+        await t.rollback()
         res.status(500).json({ err: err })
     }
 }
